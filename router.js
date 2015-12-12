50acd0262114f03cd1c99a6105c999f31534e6ae
Zentelechia <zentelechia@gmail.com>
2015-12-12 22:23:38 +0300

    1

--- router.js
+++ router.js
@@ -1,10 +1,12 @@
-Router.route('/', function () {
-	this.next();
-});
 Router.route('friends', {
 	path : "/friends",
 	onBeforeAction: function () {
-		Session.set("task_id",this.params._id);
+		this.next();
+	}
+});
+Router.route('/', {
+	path : "/",
+	onBeforeAction: function () {
 		this.next();
 	}
 });
